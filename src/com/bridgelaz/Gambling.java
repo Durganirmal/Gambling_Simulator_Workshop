c6759c11965b67b03c7fb28f6b56c30ad465c056
Durga Nirmal <durganirmal123@gmail.com>
2021-11-20 09:27:55 +0530

    Added : Start with Stake

---
+++ src/com/bridgelaz/Gambling.java
@@ -0,0 +1,7 @@
+public class Gambling {
+    public static void main (String[] args){
+        int stake_of_Gambler= 100;
+        int bet_for_Game = 1;
+
+    }
+}
---
+++ src/com/bridgelaz/Gambling_UC2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Gambling_UC2 {
+    public static void main (String[] args){
+        int stake_of_Gambler= 100;
+        int bet_for_Game = 1;
+        Random ran = new Random();
+        int i = ran.nextInt(2);
+        if (i==0){
+            System.out.println("Player won the game");
+            stake_of_Gambler=stake_of_Gambler+bet_for_Game;
+        }
+        else{
+            System.out.println("Player loss the Game");
+            stake_of_Gambler=stake_of_Gambler-bet_for_Game;
+        }
+
+    }
+}

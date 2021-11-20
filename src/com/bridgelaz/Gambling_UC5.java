7d0eef3a2095bd0d6daf997f6cf71a91cccaaa84
Durga Nirmal <durganirmal123@gmail.com>
2021-11-20 09:43:50 +0530

    Added :each month like to know the days won and lost

--- src/com/bridgelaz/Gambling_UC5.java
+++ src/com/bridgelaz/Gambling_UC5.java
@@ -1,10 +1,7 @@
-package com.bridgelaz;
-
 import java.util.Random;
 
-public class Gambler {
-
-	public static void main (String[] args) {
+public class Gambling_UC5 {
+    public static void main (String[] args) {
         int stake_of_Gambler = 100;               //Intialising amount present with Gambler before starting Game in stake_of_Gambler
         int bet_for_Game = 1;           //Intialising how much amount gamer bet in bet_for_Game
         int win =0;
